de37f703db4fb400bb68b4ae7e85ed1c891a2972
David Hernandez <dahernan@gmail.com>
2019-01-28 16:10:38 +0000

    series items into an object

--- engine/examples/python_imagenet/main.py
+++ engine/examples/python_imagenet/main.py
@@ -43,7 +43,7 @@ def to_series(classname, confidence, startMS, stopMS):
         'found': classname,
         'object':  obj
     }]
-    return seriesItems
+    return {'series': seriesItems}
 
 
 @app.route('/')
